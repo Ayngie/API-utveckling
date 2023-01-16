441cf24c625783f3a9d824c67fbd9070d920751a
Angelica Reuterswärd <angelica@macbook-pro.lan>
2023-01-16 16:52:33 +0100

    Jobbat vidare på todo och uppdaterat den med Petters kod från i fredags - där vi har createTicket tillagt

--- 3-todo-graphql/src/server.js
+++ 3-todo-graphql/src/server.js
@@ -1,3 +1,4 @@
+// Enables Node to be able to read our .env variables
 require("dotenv").config();
 const { ApolloServer } = require("@apollo/server");
 const { resolvers } = require("./resolvers");
@@ -8,12 +9,16 @@ const { startStandaloneServer } = require("@apollo/server/standalone");
 
 async function run() {
   try {
+    // Loads our schema.graphql file and reformats it for use in the next step
     const typeDefs = await loadFiles(path.join(__dirname, "schema.graphql"));
+    // Creates a schema from our typeDefs (see step above) and our resolvers
     const schema = makeExecutableSchema({
       typeDefs: typeDefs,
       resolvers: resolvers,
     });
+    // Creates a GraphQL server from our schema
     const server = new ApolloServer({ schema: schema });
+    // Starts the server in
     const res = await startStandaloneServer(server);
     console.log(`🚀 Server ready at ${res.url}`);
   } catch (error) {
